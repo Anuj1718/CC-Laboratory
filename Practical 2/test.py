f420c94c4ae2b2ec7a7c1b1d15855df785c9cf45
Anuj <anujdhole17@gmail.com>
2025-05-06 22:21:34 +0530

    Create test.py

---
+++ Practical 2/test.py
@@ -0,0 +1,15 @@
+print("Hello, World!")
+print("This is a test file.")
+
+#Copy the HTTPs link from the GitHub repository and go to cloud shell
+# Clone the repository
+
+
+# rm -rf test-repo # Remove the existing test-repo directory if it exists
+# cd/ # Change to the home directory
+# git clone HTTPS_LINK # Replace with the actual HTTPS link of your GitHub repository
+# cd test-repo # Change to the cloned repository directory
+# ls -l # List the contents of the directory
+# git status # Check the status of the repository   
+# python3 test.py # Run the test.py file
+# git add test.py # Stage the changes
